593ecc2ea5165eb811bcaac0385bc5318a72685c
GabrielC67 <gabecruzpro@gmail.com>
2026-02-19 00:05:27 -0500

    Updated gesture detection to include finger count and gesture type detection with detail.

--- gesture_detector.py
+++ gesture_detector.py
@@ -33,23 +33,22 @@ async def read_events(device):
                 gesture_state["start_y"] = 0
             elif event.code == 57 and event.value == -1:
                 gesture_state["touching"] = False
-                gesture_state["touching"] = False
                 dx = abs(gesture_state["current_x"] - gesture_state["start_x"])
                 dy = abs(gesture_state["current_y"] - gesture_state["start_y"])
 
                 if dx < 30 and dy < 30:
-                    print("Tap detected")
+                    print(f"Tap detected - {gesture_state['finger_count']} finger")
                 else:
                     if dx > dy:
                         if gesture_state["current_x"] > gesture_state["start_x"]:
-                            print("Slide right")
+                            print(f"Slide right - {gesture_state['finger_count']} finger")
                         else:
-                            print("Slide left")
+                            print(f"Slide left - {gesture_state['finger_count']} finger")
                     else:
                         if gesture_state["current_y"] > gesture_state["start_y"]:
-                            print("Slide down")
+                            print(f"Slide down - {gesture_state['finger_count']} finger")
                         else:
-                            print("Slide up")
+                            print(f"Slide up - {gesture_state['finger_count']} finger")
             elif event.code == 53:
                 gesture_state["current_x"] = event.value
                 if gesture_state["start_x"] == 0:
@@ -58,6 +57,10 @@ async def read_events(device):
                 gesture_state["current_y"] = event.value
                 if gesture_state["start_y"] == 0:
                     gesture_state["start_y"] = event.value
+            elif event.code == 325 and event.value == 1:
+                gesture_state["finger_count"] = 1
+            elif event.code == 333 and event.value == 1:
+                gesture_state["finger_count"] = 2
 
 
 
