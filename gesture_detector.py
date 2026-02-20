0bd548fc34df0548fa30f2e36b495157d81b7311
GabrielC67 <gabecruzpro@gmail.com>
2026-02-19 21:23:11 -0500

    Added a delay of time between lifting of fingers

--- gesture_detector.py
+++ gesture_detector.py
@@ -1,5 +1,6 @@
 from evdev import InputDevice, list_devices, ecodes
 import asyncio
+import time
 
 def find_trackpad():
     devices = [InputDevice(path) for path in list_devices()]
@@ -22,7 +23,8 @@ gesture_state = {
     "current_x": 0,
     "current_y": 0,
     "touching": False,
-    "current_slot": 0
+    "current_slot": 0,
+    "life_time": 0
 }
 
 async def read_events(device):
@@ -30,20 +32,9 @@ async def read_events(device):
 
         if event.type == 1 or event.type == 3:
 
-            #Code 330 (BTN_TOUCH): 1 = touching
-            if event.code == 330 and event.value == 1:
-                gesture_state["touching"] = True
-                gesture_state["start_x"] = 0
-                gesture_state["start_y"] = 0
-                gesture_state["current_slot"] = 0
-
-            #Code 47 (ABS_MT_SLOT): Which finger (0 = first, 1 = second, 2 = third)
-            elif event.code == 47:
-                gesture_state["current_slot"] = event.value
-
-            #Code 330 (BTN_TOUCH): 0 = released
-            elif event.code == 330 and event.value == 0:
-                gesture_state["touching"] = False
+            #Checking if enough time has time has passed after a lift of finger to process a gesture.
+            if gesture_state["lift_time"] > 0 and time.time() - gesture_state["lift_time"] > 0.05:
+                gesture_state["lift_time"] = 0
                 dx = abs(gesture_state["current_x"] - gesture_state["start_x"])
                 dy = abs(gesture_state["current_y"] - gesture_state["start_y"])
 
@@ -60,6 +51,22 @@ async def read_events(device):
                             print(f"Slide down - {gesture_state['finger_count']} finger")
                         else:
                             print(f"Slide up - {gesture_state['finger_count']} finger")
+
+            #Code 330 (BTN_TOUCH): 1 = touching
+            if event.code == 330 and event.value == 1:
+                gesture_state["touching"] = True
+                gesture_state["start_x"] = 0
+                gesture_state["start_y"] = 0
+                gesture_state["current_slot"] = 0
+
+            #Code 47 (ABS_MT_SLOT): Which finger (0 = first, 1 = second, 2 = third)
+            elif event.code == 47:
+                gesture_state["current_slot"] = event.value
+
+            #Code 330 (BTN_TOUCH): 0 = released
+            elif event.code == 330 and event.value == 0:
+                gesture_state["touching"] = False
+                gesture_state["lift_time"] = time.time()
 
             #Code 53 (ABS_MT_POSITION_X): Finger X position
             elif event.code == 53 and gesture_state["current_slot"] == 0:
